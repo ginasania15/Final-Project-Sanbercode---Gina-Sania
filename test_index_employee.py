1b3b5e1828281b0604da438c4510bd6f337dd79a
Galang Raka <galang.siwi@iftgroup.id>
2022-09-07 18:52:44 +0700

    add case

--- test_index_employee.py
+++ test_index_employee.py
@@ -21,6 +21,34 @@ class TestIndexEmployee(unittest.TestCase):
         message = driver.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div").text()
         self.assertEqual(message, "0253")
         time.sleep(5)
+    def test_a_search_employee_id(self):
+        driver = self.driver
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        time.sleep(5)
+        driver.find_element(by=By.NAME, value='username').send_keys('Admin')
+        time.sleep(1)
+        driver.find_element(by=By.NAME, value='password').send_keys('admin123')
+        time.sleep(1)
+        driver.find_element(By.XPATH, "//form[1]").submit()
+        driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/input').send_keys('Gina')
+        driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]').click()
+        message = driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[2]/div/span').text()
+        self.assertEqual(message, "No Records Found")
+        time.sleep(5)
+    def test_a_search_supervisor_name(self):
+        driver = self.driver
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        time.sleep(5)
+        driver.find_element(by=By.NAME, value='username').send_keys('Admin')
+        time.sleep(1)
+        driver.find_element(by=By.NAME, value='password').send_keys('admin123')
+        time.sleep(1)
+        driver.find_element(By.XPATH, "//form[1]").submit()
+        driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[5]/div/div[2]/div/div/input').send_keys('Gina')
+        driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]').click()
+        message = driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[2]/div/span').text()
+        self.assertEqual(message, "No Records Found")
+        time.sleep(5)
     def tearDown(self):
         self.driver.close()
 if __name__ == "__main__":
